21ef8663508fd46821704751f618832763d7751f
mandoolala <mjk10@kaist.ac.kr>
2021-01-17 13:31:17 +0900

    part 3 of django tutorial

--- TutorAid/urls.py
+++ TutorAid/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'TutorAid'
 urlpatterns = [
+    # ex: /courses/
     path('', views.index, name='index'),
+    # ex: /courses/5/
+    path('<int:course_id>/', views.detail, name='detail'),
+    # ex: /courses/5/results/
+    path('<int:course_id>/results/', views.results, name='results'),
+    # ex: /courses/5/vote/
+    path('<int:course_id>/vote/', views.vote, name='vote'),
 ]
--- TutorAid/views.py
+++ TutorAid/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
-# Create your views here.
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Course
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the TutorAid index")
+    latest_5_course_list = Course.objects.order_by('id')[:5]
 
+    #template = loader.get_template('courses/index.html')
+    context = {
+        'latest_5_course_list': latest_5_course_list
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'courses/index.html', context)
+
+def detail(request, course_id):
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    # except Course.DoesNotExist:
+    #     raise Http404("course does not exist")
+    # return render(request, 'courses/detail.html', {'course': course})
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'courses/detail.html', {'course': course})
+
+def results(request, course_id):
+    response = "You're looking at the results of course %s."
+    return HttpResponse(response % course_id)
+
+def vote(request, course_id):
+    return HttpResponse("You're voting on course %s." % course_id)
