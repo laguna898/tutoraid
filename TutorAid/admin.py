e8bbaa0d5e2b387c2d9b24fd6d0e661c51b98450
mandoolala <mjk10@kaist.ac.kr>
2021-01-18 12:43:04 +0900

    add model for admin page

--- TutorAid/admin.py
+++ TutorAid/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, Registration, Session, Attendance, Invoice
 from .models import Student
 
 admin.site.register(Course)
 admin.site.register(Student)
-
+admin.site.register(Registration)
+admin.site.register(Session)
+admin.site.register(Attendance)
+admin.site.register(Invoice)
